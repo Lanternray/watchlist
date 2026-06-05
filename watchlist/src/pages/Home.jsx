1f74eb80f6ca0f1348836eb069207df7bb8acf06
Shane <97196288+Lanternray@users.noreply.github.com>
2026-06-05 14:02:38 -0500

    Added logic for favorites, completed main app

--- watchlist/src/pages/Home.jsx
+++ watchlist/src/pages/Home.jsx
@@ -1,20 +1,51 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/Home.css";
 
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        { id: 1, title: "John Wick", releas_date: "2020" },
-        { id: 2, title: "Terminator", releas_date: "1999" },
-        { id: 3, title: "The Matrix", releas_date: "1998" },
-    ];
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            }
+                finally {
+                    setLoading(false)
+                }
+        }
 
-    const handleSearch = (e) => {
+        loadPopularMovies()
+    }, [])
+
+    const handleSearch = async (e) => {
         e.preventDefault()
-        alert(searchQuery)
-    }
+        if (!searchQuery.trim()) return
+        if (loading) return
+
+        setLoading(true)
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (err) {
+            console.log(err)
+            setError("Failed to search movies...")
+        } finally {
+            setLoading(false)
+        }
+
+
+        setSearchQuery("");
+    };
 
     return (
         <div className="home">
@@ -27,6 +58,10 @@ function Home() {
                 <button type="submit" className="search-button">Search</button>
             </form>
 
+                {error && <div className="error-message">{error}</div>}
+
+            {loading ? <div className="loading">Loading... </div>
+            : (
             <div className="movies-grid">
                 {movies.map(
                     (movie) => 
@@ -35,6 +70,7 @@ function Home() {
                         )
                 )}
             </div>
+            )}
         </div>
     );
 }
